295e69eec9473d09ffc8d5be7808f986463050e3
tlsghdrms <tlsghdrms00@naver.com>
2025-11-21 16:32:13 +0900

    팝업창 표시

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,12 +11,20 @@ const authMiddleware = asyncHandler(async(req, res, next) => {
             req.user = await User.findById(decode.id).select("-password");
             next();
         } catch (error) {
-            res.status(401);
-            throw new Error("접근 권한이 없습니다. (Invalid token)");
+            res.send(
+                `<script type="text/javascript">
+                    alert('인증 세션이 만료되었습니다. 다시 로그인해주세요.');
+                    window.location.href = '/users/login';
+                </script>`
+            );
         }
     } else {
-        res.status(401);
-        throw new Error("접근 권한이 없습니다. (No token)");
+        res.send(
+            `<script type="text/javascript">
+                alert('로그인이 필요한 서비스입니다.');
+                window.location.href = '/users/login';
+            </script>`
+        );
     }
 });
 
